50c699be206fc504791fd3210d72eb197036cd9a
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2023-05-25 16:08:58 -0300

    Mistake found
    the import for the Provider on the layout.jsx file was wrong

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,6 @@
 import '@styles/globals.css'
 import Nav from '@components/Nav'
-import Provider from '@components/Nav'
-
-
+import Provider from '@components/Provider'
 
 export const metadata = {
     title: "Promptopia",
@@ -13,6 +11,7 @@ const RootLayout = ({ children }) => {
   return (
     <html lang='en'>
         <body>
+        <Provider>
             <div className='main'>
                 <div className='gradient' />
             </div>
@@ -20,6 +19,7 @@ const RootLayout = ({ children }) => {
                 <Nav />
                 {children}
             </main>
+        </Provider>
         </body>
     </html>
   )
