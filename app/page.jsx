243255e66530d72b71c65827b1de44f910d93888
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2023-05-25 01:19:29 -0300

    Components structure created
    Adding it to the home page

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <section className='w-full flex-center flex-col'>
+            <h1 className='head_text text-center'>
+                Discovery & Share 
+                <br className='max-md:hidden'/>
+                <span className='orange_gradient text-center'> AI-Powered Prompts
+                </span>
+            </h1>
+            <p>
+                Proptopia is an open-source AI prompting tool for modern world to dicover, vreate and share creative prompts.
+            </p>
+
+            {/* {feed} */}
+        </section>
     )
 }
 
